6957fcb2cc2793963d26c6e70f46e5ca13f9a0b2
Folarin Isaac <folarinisaac36@gmail.com>
2020-09-07 21:23:31 +0100

    created the fragments and made some changes to the designs

--- app/src/main/java/com/example/gadsapp/MainActivity.java
+++ app/src/main/java/com/example/gadsapp/MainActivity.java
@@ -1,14 +1,57 @@
 package com.example.gadsapp;
 
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toolbar;
+
+import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
+    private Toolbar toolbar;
+    private Button button;
+    TabLayout tabLayout;
+    ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        toolbar = findViewById(R.id.toolbar_leaderboard);
+        setSupportActionBar(toolbar);
+
+        viewPager = findViewById(R.id.viewPager);
+        tabLayout = findViewById(R.id.tabs);
+        button = findViewById(R.id.submit_button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SubmitActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        TablayoutAdapter householdPagerAdapter = new TablayoutAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(householdPagerAdapter);
+        tabLayout.setupWithViewPager(viewPager);
+
+
+
+
+        }
+
+    private void setSupportActionBar(Toolbar toolbar) {
     }
+
+
 }
---
+++ app/src/main/java/com/example/gadsapp/TablayoutAdapter.java
@@ -0,0 +1,53 @@
+package com.example.gadsapp;
+
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+public class TablayoutAdapter extends FragmentPagerAdapter {
+
+    // constructor
+    public TablayoutAdapter(@NonNull FragmentManager fm) {
+        super(fm);
+    }
+
+    // Switching between the fragments
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        switch (position){
+            case 0:
+                LearningLeadersFragment learningLeadersFragment = new LearningLeadersFragment();
+                return learningLeadersFragment;
+            case 1:
+                SkillLeadersFragment skillLeadersFragment = new SkillLeadersFragment();
+                return skillLeadersFragment;
+            default:
+                return null;
+        }
+    }
+
+    // sets the title for each fragment
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        CharSequence title;
+        if (position == 0){
+            title = "Learning Leaders";
+        }else {
+            title = "SkillIQ Leaders";
+        }
+        return title;
+    }
+
+    // This counts the total number of tabs available
+    @Override
+    public int getCount() {
+
+        return 2;
+    }
+}
